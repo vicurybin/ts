deb5674f17788b0ec10dc11aeb29cb78581d71d1
domob <fanbin@domob.cn>
2017-08-02 18:57:12 +0800

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
+<template>
+  <div id='app'>
+    <div @click="clicktest">点击测试</div>
+    <div >{{computedtest}}</div>
+    <img src='./assets/logo.png'>
+    <router-view></router-view>
+  </div>
+</template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 @Component({
   // 所有的组件选项都可以放在这里
-  template: "<div id='app'><img src='./assets/logo.png'><router-view></router-view></div>",
-  name: 'app'
+  props:{
+    propmsg:String
+  },
+  name:'lalal',
+  data(){
+    return {
+      age:'111'
+    }
+  }
 })
 export default class App extends Vue {
-  
+  msg:number = 133;
+  get computedtest (){
+    return 'computed'+this.msg;
+  }
+  clicktest (): void {
+      alert('msg'+this.msg)
+  }
 } 
 </script>
 
